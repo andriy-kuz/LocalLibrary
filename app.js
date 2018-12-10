c4d88a7498e4f1c9c82d3a2f9731ddc599413101
andrii.kuz@perfectial.com <jSHovpesfifa13>
2018-12-10 22:52:54 +0200

    Add layout template.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter   = require('./routes/index');
 var usersRouter   = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
+
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1:27017/local_library';
